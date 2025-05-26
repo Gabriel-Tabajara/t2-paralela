f387d512185a9f8e8240926dad395390ea5cc48f
gischenato <gischenato@gmail.com>
2025-05-25 21:39:01 -0300

    bbb

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 
 # Default number of processes
 NUM_PROCESSES=${1:-4}
+CITIES =${2:-10}
 
-mpicc tspBruteForceParallel.c -o tspBruteForceParallel -lm && mpirun -np $NUM_PROCESSES ./tspBruteForceParallel
+mpicc tspBruteForceParallel.c -o tspBruteForceParallel -lm && mpirun -np $NUM_PROCESSES ./tspBruteForceParallel $CITIES
